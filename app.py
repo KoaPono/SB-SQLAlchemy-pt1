fc54ea80fd58dc96b27e8b4132b7265bbd6ad2bc
Kelii DeLeon <keliikeideleon@gmail.com>
2023-10-20 09:50:37 -0600

    Part One Complete

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 """Blogly application."""
 
 from flask import Flask, request, redirect, render_template
-from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'bananas'
 
-toolbar = DebugToolbarExtension(app)
-
-connect_db(app)
-db.create_all()
+with app.app_context():
+    connect_db(app)
+    db.create_all()
 
 @app.route('/')
 def root():
@@ -32,12 +30,14 @@ def users_new_form():
     return render_template('new.html')
 
 @app.route('/users/new', methods=["POST"])
-def users_new_form():
+def users_new():
     """Shows a form to create a new user"""
     new_user = User(
-        first_name=request.form['first_name'],
-        last_name=request.form['last_name'],
-        image_url=request.form['image_url'] or None)
+        first_name=request.form['first-name'],
+        last_name=request.form['last-name'],
+        image_url=request.form['image-url'] or None)
+    
+    print(f"User: {new_user}")
     
     db.session.add(new_user)
     db.session.commit()
@@ -63,9 +63,9 @@ def users_update(user_id):
     """Handle form submission for updating an existing user"""
 
     user = User.query.get_or_404(user_id)
-    user.first_name = request.form['first_name']
-    user.last_name = request.form['last_name']
-    user.image_url = request.form['image_url']
+    user.first_name = request.form['first-name']
+    user.last_name = request.form['last-name']
+    user.image_url = request.form['image-url']
 
     db.session.add(user)
     db.session.commit()
